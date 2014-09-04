b8f76abda468036a0b932295b0041766e3292413
Marc <marc.roedder@finanzen100.de>
2014-09-04 17:59:30 +0200

    initial

---
+++ Pod/Classes/STKWebKitViewController.m
@@ -0,0 +1,150 @@
+//
+//  STKWebKitViewController.m
+//  FOCUS
+//
+//  Created by stickbook  on 03.09.14.
+//  Copyright (c) 2014 Finanzen100 GmbH. All rights reserved.
+//
+
+#import "STKWebKitViewController.h"
+
+@interface STKWebKitViewController ()
+
+@property(nonatomic) NSMutableArray *viewConstraints;
+@property(nonatomic) UIColor *savedNavigationbarTintColor;
+@property(nonatomic) UIColor *savedToolbarTintColor;
+
+@end
+
+@implementation STKWebKitViewController
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.webView = [[WKWebView alloc] initWithFrame:CGRectZero];
+        [self.view addSubview:self.webView];
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    NSAssert(self.navigationController, @"STKWebKitViewController needs to be contained in a UINavigationController. If you are presenting STKWebKitViewController modally, use STKModalWebKitViewController instead.");
+
+    [self.view setNeedsUpdateConstraints];
+    [self.navigationController setToolbarHidden:NO animated:YES];
+    [self fillToolbar];
+    
+    self.savedNavigationbarTintColor = self.navigationController.navigationBar.barTintColor;
+    self.savedToolbarTintColor = self.navigationController.toolbar.barTintColor;
+    
+    if (self.toolbarTintColor) {
+        self.navigationController.toolbar.barTintColor = self.toolbarTintColor;
+        self.navigationController.toolbar.backgroundColor = self.toolbarTintColor;
+        self.navigationController.toolbar.tintColor = [UIColor whiteColor];
+    }
+    if (self.navigationBarTintColor) {
+        self.navigationController.navigationBar.barTintColor = self.navigationBarTintColor;
+    }
+    
+    [self addObserver:self forKeyPath:@"webView.title" options:NSKeyValueObservingOptionNew context:NULL];
+    [self addObserver:self forKeyPath:@"webView.loading" options:NSKeyValueObservingOptionNew context:NULL];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    self.navigationController.navigationBar.barTintColor = self.savedNavigationbarTintColor;
+    self.navigationController.toolbar.barTintColor = self.savedToolbarTintColor;
+
+    [self removeObserver:self forKeyPath:@"webView.title"];
+    [self removeObserver:self forKeyPath:@"webView.loading"];
+}
+
+- (void)fillToolbar
+{
+    UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back"] style:UIBarButtonItemStylePlain target:self action:@selector(backTapped:)];
+    if (self.webView.canGoBack) {
+        backItem.tintColor = nil;
+    } else {
+        backItem.tintColor = [UIColor lightGrayColor];
+    }
+    
+    UIBarButtonItem *forwardItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"forward"] style:UIBarButtonItemStylePlain target:self action:@selector(forwardTapped:)];
+    if (self.webView.canGoForward) {
+        forwardItem.tintColor = nil;
+    } else {
+        forwardItem.tintColor = [UIColor lightGrayColor];
+    }
+    
+    UIBarButtonItem *reloadItem;
+    if (self.webView.isLoading) {
+        reloadItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"stop"] style:UIBarButtonItemStylePlain target:self action:@selector(stopTapped:)];
+    } else {
+        reloadItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"refresh"] style:UIBarButtonItemStylePlain target:self action:@selector(reloadTapped:)];
+    }
+    UIBarButtonItem *shareItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(shareTapped:)];
+    UIBarButtonItem *flexibleSpaceItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+
+    [self setToolbarItems:@[flexibleSpaceItem, backItem, flexibleSpaceItem, forwardItem, flexibleSpaceItem, reloadItem, flexibleSpaceItem, shareItem, flexibleSpaceItem] animated:NO];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:@"webView.title"]) {
+        self.title = change[@"new"];
+    } else if ([keyPath isEqualToString:@"webView.loading"]) {
+        [self fillToolbar];
+    }
+}
+
+- (void)viewDidLayoutSubviews
+{
+    self.webView.frame = self.view.bounds;
+}
+
+//- (void)updateViewConstraints
+//{
+//    if (self.viewConstraints) {
+//        [self.view removeConstraints:self.viewConstraints];
+//    }
+//    self.viewConstraints = [NSMutableArray array];
+//    
+//    [self.view addConstraints:self.viewConstraints];
+//    [super updateViewConstraints];
+//}
+
+- (void)backTapped:(UIBarButtonItem *)button
+{
+    [self.webView goBack];
+}
+
+- (void)forwardTapped:(UIBarButtonItem *)button
+{
+    [self.webView goForward];
+}
+
+- (void)reloadTapped:(UIBarButtonItem *)button
+{
+    [self.webView reload];
+}
+
+- (void)stopTapped:(UIBarButtonItem *)button
+{
+    [self.webView stopLoading];
+}
+
+- (void)shareTapped:(UIBarButtonItem *)button
+{
+    
+}
+
+@end
